15e6d63c832d866d29932f97acaac8c9102e54d8
Abhishek-Puri-Goswami <ap.goswami.2854@gmail.com>
2026-04-01 06:19:19 +0530

    [Abhishek Puri Goswami] : UC18 - update AuthResponse structure for JWT authentication response

--- src/main/java/com/app/quantitymeasurement/dto/response/AuthResponse.java
+++ src/main/java/com/app/quantitymeasurement/dto/response/AuthResponse.java
@@ -1,5 +1,6 @@
 package com.app.quantitymeasurement.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -42,6 +43,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AuthResponse {
 
     /**
